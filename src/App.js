dc8a02eda944333da4dd462ef096d425fd88b8c0
Julien Picaud <julienpicaud83@gmail.com>
2023-12-16 13:13:54 +0100

    avancement index.html

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Components/header/header";
+
+function App() {
+  return (
+    <div className="Home">
+      <h1>lol.</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
