9c1b7c8f113656cd0ee2965ae67e853849b8b288
Julien Picaud <julienpicaud83@gmail.com>
2023-12-27 16:34:50 +0100

    page 404 ok

--- src/Pages/Error/error.jsx
+++ src/Pages/Error/error.jsx
@@ -1,7 +1,23 @@
-function error (){
-    return (
-        <div><h1>Erreur 404</h1>
+import './error.scss'
+import { Link } from 'react-router-dom'
+import Header from '../../Components/header/header'
+import Footer from '../../Components/footer/footer'
+
+function error() {
+      return (
+            <div className="notFound">
+                  <Header />
+                  <div className="notFound_infos">
+                        <h1 className="notFound_infos_title">404</h1>
+                        <p className="notFound_infos_content">
+                              La page que vous recherchez n'existe pas.
+                        </p>
+                  </div>
+                  <Link className="notFound_infos_return" to="/">
+                        Retourner sur la page d'accueil
+                  </Link>
+                  <Footer />
             </div>
-        )
-    }
-    export default error
+      )
+}
+export default error
