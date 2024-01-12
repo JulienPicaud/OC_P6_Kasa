6d2ecc322d9b3a7fd9fb6984f90996cabde7cce6
Julien Picaud <julienpicaud83@gmail.com>
2024-01-12 17:26:01 +0100

    page accomodation en cours

--- src/Pages/Accomodations/accomodation.jsx
+++ src/Pages/Accomodations/accomodation.jsx
@@ -1,10 +1,37 @@
-//import datas from '../../data/data'
+import datas from '../../assets/datas'
+import './accomodation.scss'
+import Header from "../../Components/header/header";
+import Footer from "../../Components/footer/footer";
+import Collapse from "../../Components/collapse/collapse";
+// import Slider from "../../Components/slider/slider";
+import { useParams } from "react-router-dom";
 
 
-function accomodations (){
+
+export default function Accomodations (){
+// Hook useParams pour récuperer l'ID dans l'url
+	const UrlIdAccomodation = useParams('id').id;
+// On fait correspondre l'id de l'url avec l'id dans le tableau de donnéees data
+    const CurrentAccomodation = datas.filter(data => data.id === UrlIdAccomodation);
+// Est ce que je dois utiliser un useeffect 
+
+    const hostName = CurrentAccomodation[0].host.name.split(' '); 
+	const rating = CurrentAccomodation[0].rating;
+    const description  = CurrentAccomodation[0].description;
+	const equipments = CurrentAccomodation[0].equipments;
+	const title = CurrentAccomodation[0].title;
+    const location = CurrentAccomodation[0].location;
+
     return (
-        <div><h1>Logements</h1>
-            </div>
+        <>
+        < Header/>
+        
+        <h1>{title}</h1>
+        <p>{location}</p>
+
+        <script>console.log(UrlIdAccomodation)</script>
+        <Collapse />
+        < Footer/>
+            </>
         )
     }
-    export default accomodations
