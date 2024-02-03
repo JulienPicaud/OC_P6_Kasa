c9fd29751639f6bac91e51cb3a193f74d1618035
Julien Picaud <julienpicaud83@gmail.com>
2024-02-03 09:08:25 +0100

    clean rooter and app.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from './Pages/Home/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import About from './Pages/About/about'
+import Error from './Pages/Error/error'
+import Accomodations from './Pages/Accomodations/accomodation'
+
+
+export default function App (){
+    return(
+            <Router>
+                  <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/*" element={<Error />} />
+                        <Route path="/accomodation/:id" element={<Accomodations />} />
+                  </Routes>
+            </Router>
+)}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Pages/Home/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import About from './Pages/About/about'
-import Error from './Pages/Error/error'
-import Accomodations from './Pages/Accomodations/accomodation'
+import App from './App'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-            <Router>
-                  <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/*" element={<Error />} />
-                        <Route path="/accomodation/:id" element={<Accomodations />} />
-                  </Routes>
-            </Router>
-,
+< App />
 )
